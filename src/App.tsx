8434e04b10167488356d7811a040c7e15269bafc
Saurabh Dabas <developerdabas@gmail.com>
2022-06-13 16:51:33 +0530

    passing props to user-input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>("");
   return (
     <div className="App">
       <span className='heading'>DAILY PLANNER</span>
-      <InputField/>
+      <InputField task={task} setTask={setTask}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./styles.css";
-const InputField: React.FC = () => {
+
+interface Props {
+  task : string,
+  setTask : React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField: React.FC<Props> = ({ task, setTask}) => {
+
   return (
-    <form className = "user-input">
-      <input className = "user-input__box" type="input" placeholder='Add a task to get started'/>
+    <form className = "user-input" >
+      <input className = "user-input__box" type="input" placeholder='Add a task to get started' value={task} onChange={(event)=>setTask(event.target.value)}/>
       <button className="user-input__submit" type="submit">+</button>
     </form>
   )
