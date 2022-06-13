f3ad7a650d7d0e2696b648a93185386ec6e20758
Saurabh Dabas <developerdabas@gmail.com>
2022-06-13 16:25:10 +0530

    import input field component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <span className='heading'>DAILY PLANNER</span>
+      <InputField/>
     </div>
   );
 }
