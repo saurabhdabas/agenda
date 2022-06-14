b1e5ce472ed884ca1b4154836cd28d46ac01a5c8
Saurabh Dabas <developerdabas@gmail.com>
2022-06-14 15:17:29 +0530

    Add styling to active tasks and completed tasks

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -12,9 +12,20 @@ interface Props {
 const singleTask: React.FC<Props> = ({id, task, isDone,deleteTask}) => {
   return (
     <div className="task">
-      <div className="task__description">{task}</div>
-      <div>{isDone ?  ""  :<BsPatchCheckFill/>}</div>
-      <div className="task__description"><AiFillDelete onClick={()=>deleteTask(id)}/></div>
+      <div className="task__description">
+        {task}
+      </div>
+      <div className="task__icons">
+      <div>
+        {isDone ?  ""  :<AiFillEdit/>}
+      </div>
+      <div>
+        {isDone ? "" : <AiFillDelete onClick={()=>deleteTask(id)}/>}
+      </div>
+      <div>
+        {isDone ?  ""  :<BsPatchCheckFill/>}
+      </div>
+      </div>
     </div>
   )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -10,7 +10,21 @@ interface Props {
 const Tasks:React.FC<Props> = ({tasks,deleteTask}) => {
   return (
     <div className="container">
-      <div>
+      <div className="container__active-tasks">
+        <h2>ACTIVE TASKS</h2>
+        {tasks?.map((action, index) => (
+          <SingleTask
+            key={action.id}
+            id={action.id}
+            task={action.task}
+            isDone={action.isDone}
+            deleteTask={deleteTask}
+          />
+          ))
+        }
+      </div>
+      <div className="container__completed-tasks">
+        <h2>COMPLETED TASKS</h2>
         {tasks?.map((action, index) => (
           <SingleTask
             key={action.id}
